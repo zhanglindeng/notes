d905e111a32f48f51deaafaf7ccbf8159a499e4f
zhanglindeng <zhanglindeng@outlook.com>
2016-08-16 01:42:44 +0800

    Update date.php

--- php/date.php
+++ php/date.php
@@ -39,8 +39,8 @@ $endDate = '2016-04-24 14:00:00';
 $startDate = '2016-04-22 14:00:00';
 $endDate = '2016-04-27 12:00:00';
 
-$startDate = '2016-04-22 09:00:00';
-$endDate = '2016-05-22 18:00:00';
+$startDate = '2016-09-13 09:00:00';
+$endDate = '2016-09-14 14:00:00';
 
 $dates = MyDate::divDate(strtotime($startDate),strtotime($endDate));
 
@@ -54,6 +54,9 @@ $holidays = [
     '2016-04-30',
     '2016-05-01',
     '2016-05-02',
+    '2016-09-15',
+    '2016-09-16',
+    '2016-09-17',
 ];
 
 // 交集
@@ -82,7 +85,6 @@ $workOnWeeks = [
     '2016-09-18',
     '2016-10-08',
     '2016-10-09',
-    '2016-05-14',// 假设的周末上班日
 ];
 
 // 要除去到周末日期
@@ -93,4 +95,45 @@ var_dump($weeks);
 $dates = array_diff($dates, $weeks);
 
 // 实际上班日
+$dates = array_values($dates);
 var_dump($dates);
+
+$days = 0;
+$total = count($dates);
+if($total > 0) {
+
+    // 12点之前到当作是一天
+    $point = strtotime($dates[0].' 12:00:00');
+    if(strtotime($startDate) < $point) {
+        $days = 1;
+    } else {
+        $days = 0.5;
+    }
+    echo $dates[0].'==>'.$days.';;;';
+    // todo 添加第一天的数据
+
+    // 最后一天
+    $last = $total-1;
+    if( $last > 0) {
+        $point = strtotime($dates[$last].' 14:00:00');
+        if(strtotime($endDate) > $point) {
+            $days = 1;
+        } else {
+            $days = 0.5;
+        }
+        echo $dates[$last].'==>'.$days.';;;';
+        // todo 添加最后一天到数据
+        // 把最后一个删除掉
+        array_pop($dates);
+    }
+
+    // 删掉第一个
+    array_shift($dates);
+
+    // 中间到全是1
+    foreach($dates as $date) {
+        $days = 1;
+        echo $date.'==>'.$days.';;;';
+    }
+
+}
