e9308440a027c92c7620777d14edbdcd3c25e3fc
zhanglindeng <zhanglindeng@outlook.com>
2017-06-10 21:18:22 +0800

    install_docker.sh

---
+++ docker/install_docker.sh
@@ -0,0 +1,23 @@
+apt-get remove docker docker-engine
+
+apt-get update
+
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+apt-get update
+
+apt-get install -y docker-ce
+
+docker run hello-world
+
+curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
