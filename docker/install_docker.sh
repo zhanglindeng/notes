7fc9735913adf338ee2082a26b71ecbdfd6bef2f
zhanglindeng <zhanglindeng@outlook.com>
2017-09-16 08:44:27 +0800

    uninstall docker

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -21,3 +21,8 @@ docker run hello-world
 curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
+
+uninstall
+
+sudo apt-get purge docker-ce
+sudo rm -rf /var/lib/docker
